7c82df58f89539701d0e169ed808aa97787439cf
ShadowBurn1210 <leo.saulitis@gmail.com>
2023-01-13 15:39:45 +0200

    Lecture

--- src/main/scala/LectureQuestions/Lecture3.scala
+++ src/main/scala/LectureQuestions/Lecture3.scala
@@ -3,7 +3,7 @@ package LectureQuestions
 object Lecture3 {
 
   def main(args: Array[String]): Unit = {
-    for (i <- 1 to 10){
+    for (i <- 2 to 10){
       println(i)
     }
 //    var contents = ""
