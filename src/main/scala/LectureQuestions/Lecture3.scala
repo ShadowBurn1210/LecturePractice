0ef967585b3ad6876d9edfd195d176381ccf9d2e
ShadowBurn1210 <leo.saulitis@gmail.com>
2023-01-13 15:37:15 +0200

    Lecture 13/01

---
+++ src/main/scala/LectureQuestions/Lecture3.scala
@@ -0,0 +1,13 @@
+package LectureQuestions
+
+object Lecture3 {
+
+  def main(args: Array[String]): Unit = {
+    for (i <- 1 to 10){
+      println(i)
+    }
+//    var contents = ""
+//    contents = contents + line + "\n"
+  }
+
+}
